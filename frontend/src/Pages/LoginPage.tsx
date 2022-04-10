fc468896ba456323de3c10edc5662687e8ded83e
Filip <szymanski.fp@gmail.com>
2022-04-10 15:26:50 +0200

    finished LoginPage

---
+++ frontend/src/Pages/LoginPage.tsx
@@ -0,0 +1,18 @@
+import LoginComponent from "../Components/LoginComponent";
+
+function LoginPage(){
+    return(
+        <div
+        className="
+        flex
+        justify-center
+        items-center
+        h-screen
+        w-screen
+        ">
+            <LoginComponent />
+        </div>
+    )
+}
+
+export default LoginPage;
