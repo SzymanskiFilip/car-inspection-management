97d1986d28043776c8e0c45cea0829307881e602
Filip <szymanski.fp@gmail.com>
2022-04-12 22:01:27 +0200

    rendering page based on the authenticated context

--- frontend/src/Utils/BlockAuth.tsx
+++ frontend/src/Utils/BlockAuth.tsx
@@ -1,11 +1,25 @@
 import {AuthContext} from "../Context/AuthContext";
 import {useContext} from "react";
-import React, {ReactNode} from "react";
+import { Navigate } from "react-router-dom";
+import React, {FC} from "react";
 import LoginPage from "../Pages/LoginPage";
+import { AuthContextInterface } from "../Interfaces/AuthContextInterface";
 
 
-function BlockAuth({children} : {children : JSX.Element}){
-    return children;
+function BlockAuth({children} : {children : JSX.Element}) : JSX.Element{
+
+    const context = useContext(AuthContext);
+    console.log(context?.authenticated);
+
+    if(context?.authenticated === true){
+        return (
+            <Navigate to="/"/>
+        )
+    } else {
+        return (
+            children
+        )
+    }
 }
 
 export default BlockAuth;
