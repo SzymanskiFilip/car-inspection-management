e085f70f46d93b844113fe8ceb3f54485f7f4c24
Filip <szymanski.fp@gmail.com>
2022-04-12 22:01:04 +0200

    created AuthContext of tyhpe AuthContextInterface

--- frontend/src/Context/AuthContext.tsx
+++ frontend/src/Context/AuthContext.tsx
@@ -1,3 +1,5 @@
 import {createContext} from "react";
+import { AuthContextInterface } from "../Interfaces/AuthContextInterface";
 
-export const AuthContext = createContext({});
+
+export const AuthContext = createContext<AuthContextInterface | null>(null);
