a8b42bb80893cf1cc09950847d146d02a4256ea1
Filip <szymanski.fp@gmail.com>
2022-04-13 17:27:40 +0200

    added inspections page with nav

--- frontend/src/Pages/InspectionsPage.tsx
+++ frontend/src/Pages/InspectionsPage.tsx
@@ -7,8 +7,15 @@ function InspectionsPage(): JSX.Element {
 
     return(
         <div>
-            <nav>
-                Hello, {context?.username}
+            <nav className="flex justify-between items-center h-10 bg-gradient-to-r from-indigo-500 to-indigo-700">
+                <h1 className="mx-2 text-white">Hello, {context?.username}!</h1>
+                <div className="flex justify-center items-center justify-self-end">
+
+                    <button className="nav-button">Add Inspection</button>
+
+                    <button className="nav-button">Account</button>
+                    <button className="nav-button">Logout</button>
+                </div>
             </nav>
         </div>
     )
