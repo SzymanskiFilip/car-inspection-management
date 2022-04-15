d672e3be46fece138d017368c57cd87b587bf1dc
Filip <szymanski.fp@gmail.com>
2022-04-15 16:51:09 +0200

    styled and added link adressess

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function HomePage():JSX.Element{
     return(
-        <div className="flex justify-center items-center h-screen w-screen bg-black">
+        <div className="flex justify-center items-center h-screen w-screen bg-sky-500">
             <Link to="/login" className="hp-link">Login</Link>
-            <Link to="/" className="hp-link">Register</Link>
-            <Link to="/" className="hp-link">Pricing</Link>
-            <Link to="/" className="hp-link">Information</Link>
-            <Link to="/" className="hp-link">Contact</Link>
+            <Link to="/register" className="hp-link">Register</Link>
+            <Link to="/prices" className="hp-link">Pricing</Link>
+            <Link to="/information" className="hp-link">Information</Link>
+            <Link to="/contact" className="hp-link">Contact</Link>
         </div>
     )
 }
