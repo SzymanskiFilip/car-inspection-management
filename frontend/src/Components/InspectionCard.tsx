0348355a6272c1a39636ec8f9ab739adc0427144
Filip <szymanski.fp@gmail.com>
2022-04-15 23:22:41 +0200

    fixed list styling

--- frontend/src/Components/InspectionCard.tsx
+++ frontend/src/Components/InspectionCard.tsx
@@ -3,20 +3,25 @@ import InspectionListRow from "./InspectionListRow";
 
 function InspectionCard(): JSX.Element {
     return(
-        <div className="inspection flex flex-col">
+        <div className="inspection flex flex-col container">
             <div className="flex flex-row mx-2 my-2">
-                <div className="w-full h-full">
+                <div className="w-full">
                     <h1>List:</h1>
-                    <div className="flex flex-col">
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
+                    <InspectionListRow todo="CHANGE TIRES" status="DONE"/>
 
-                        <InspectionListRow todo="hello" status="hello"/>
 
-
-                    </div>
                 </div>
             </div>
 
-            <div className="w-full h-full flex flex-row justify-around my-5 text-stone-900">
+            <div className="w-full flex flex-row justify-around my-5 text-stone-900">
                 <h1>Mercedes-Benz</h1>
                 <h1>C63 AMG</h1>
                 <h1>2014</h1>
