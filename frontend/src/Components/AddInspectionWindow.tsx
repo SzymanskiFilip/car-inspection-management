46d8ab0c25b68aeec5c66cec3658eeb5e673aeed
Filip <szymanski.fp@gmail.com>
2022-04-16 22:29:48 +0200

    Added a inspectionwindow and centered it

---
+++ frontend/src/Components/AddInspectionWindow.tsx
@@ -0,0 +1,9 @@
+function AddInspectionWindow(): JSX.Element {
+    return(
+        <div className="inspection-window">
+            1
+        </div>
+    )
+}
+
+export default AddInspectionWindow;
--- frontend/src/Pages/InspectionsPage.tsx
+++ frontend/src/Pages/InspectionsPage.tsx
@@ -1,11 +1,13 @@
 import { AuthContext } from "../Context/AuthContext";
-import {useContext } from "react";
+import {useContext, useState } from "react";
 import {Link} from "react-router-dom"
 import InspectionCard from "../Components/InspectionCard";
+import AddInspectionWindow from "../Components/AddInspectionWindow";
 
 function InspectionsPage(): JSX.Element {
 
     const context = useContext(AuthContext);
+    const [windowTrigger, setWindowTrigger] = useState<Boolean>(false);
 
     return(
         <div>
@@ -13,16 +15,14 @@ function InspectionsPage(): JSX.Element {
                 <h1 className="mx-2 text-white">Hello, {context?.username}!</h1>
                 <div className="flex justify-center items-center justify-self-end">
 
-                    {/* Create pop-up wih inspection creation */}
                     <button className="nav-button">Add Inspection</button>
-
-                    {/* navigate to account page */}
                     <Link to="/account" className="nav-button">Account</Link>
-
-                    {/* logout, navigate to home page */}
                     <button className="nav-button">Logout</button>
+
                 </div>
             </nav>
+
+            <AddInspectionWindow />
 
             
             <div className="w-screen flex flex-col justify-center items-center">
