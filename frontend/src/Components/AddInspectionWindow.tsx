c8da86d9598453d095ba4a04452b4cf761f78dea
Filip <szymanski.fp@gmail.com>
2022-04-17 14:02:23 +0200

    fixed select list

--- frontend/src/Components/AddInspectionWindow.tsx
+++ frontend/src/Components/AddInspectionWindow.tsx
@@ -4,9 +4,29 @@ interface props {
 }
 
 function AddInspectionWindow({windowTrigger, setWindowTrigger}: props): JSX.Element {
+
+    const options: string[] = ["Standard", "Premium", "Sport"];
+    const listItems = options.map((option) => {
+        <option value={option}>hi</option>
+    });
+
     return(
-        <div className="inspection-window">
-            <h1>Request new Inspection123</h1>
+        <div className="inspection-window h-96">
+            <h1 className="text-center">Request new Inspection</h1>
+            <input type="text" placeholder="Make"/>
+            <input type="text" placeholder="Model"/>
+            <input type="text" placeholder="Year"/>
+            <br/>
+
+            <select>
+                {
+                    options.map(option => {
+                        return(
+                           <option value={option}>{option}</option> 
+                        )
+                    })
+                }
+            </select>
         </div>
     )
 }
