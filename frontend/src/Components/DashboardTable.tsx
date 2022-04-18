d59ef4f01d4f6339ab25888177469a5e88b172b6
Filip <szymanski.fp@gmail.com>
2022-04-18 18:18:21 +0200

    moved the table to a separate component

--- frontend/src/Components/DashboardTable.tsx
+++ frontend/src/Components/DashboardTable.tsx
@@ -1,27 +1,67 @@
+interface dashboardData {
+    id: number;
+    brand: string;
+    model: string;
+    dueDate: string;
+    year: number;
+}
+
+const row1: dashboardData = {
+    id: 1,
+    brand: "BMW",
+    model: "M4",
+    dueDate: "18.04.2022",
+    year: 2014,
+}
+
+const row2: dashboardData = {
+    id: 4,
+    brand: "AUDI",
+    model: "R8",
+    dueDate: "18.04.2022",
+    year: 2014,
+}
+
+let mockRows: dashboardData[] = [row1, row2];
+
+
 function DashboardTable(): JSX.Element{
+
+    {/* TOGGLES AND CHANGES ROW COLOR */}
+    let darkRow: Boolean = false;
+
     return(
         <table className="table-style">
                     <thead>
                         <tr>
                             <th className="table-head-cell">Brand</th>
+                            <th className="table-head-cell">Model</th>
                             <th className="table-head-cell">Due Date</th>
                             <th className="table-head-cell">Year</th>
-                            <th className="table-head-cell">Status</th>
-                            <th className="table-head-cell">Details(BTN)</th>
+                            <th className="table-head-cell">Details</th>
                             <th className="table-head-cell">Finish</th>
                         </tr>
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td className="table-cell">12333333333333</td>
-                            <td className="table-cell">12333333333333</td>
-                            <td className="table-cell">12333333333333</td>
-                            <td className="table-cell">12333333333333</td>
-                            <td className="table-cell"><button>123</button></td>
-                            <td className="table-cell"><button>finish</button></td>
-                        </tr>
+                    {
+                    mockRows.map((row) => {
+
+                        return(
+                            <tr>
+                                <td className="table-cell">{row.brand}</td>
+                                <td className="table-cell">{row.model}</td>
+                                <td className="table-cell">{row.dueDate}</td>
+                                <td className="table-cell">{row.year}</td>
+                                <td className="table-cell">OPEN</td>
+                                <td className="table-cell">FINISH</td>
+                            </tr>
+                        )
+                    })
+                    }
                     </tbody>
+                    
+
                 </table>
     )
 }
