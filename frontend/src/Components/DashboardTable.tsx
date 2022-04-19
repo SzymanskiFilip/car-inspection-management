a3b94a6c9804e2cb4a2f8713befa05771316a2f4
Filip <szymanski.fp@gmail.com>
2022-04-19 17:39:15 +0200

    adding black background on details click

--- frontend/src/Components/DashboardTable.tsx
+++ frontend/src/Components/DashboardTable.tsx
@@ -1,3 +1,6 @@
+import BlackBackground from "./BlackBackground";
+import {useState} from "react";
+
 interface dashboardData {
     id: number;
     brand: string;
@@ -14,22 +17,8 @@ const row1: dashboardData = {
     year: 2014,
 }
 
-const row2: dashboardData = {
-    id: 4,
-    brand: "AUDI",
-    model: "R8",
-    dueDate: "18.04.2022",
-    year: 2014,
-}
-const row3: dashboardData = {
-    id: 2,
-    brand: "AUDI",
-    model: "R8",
-    dueDate: "18.04.2022",
-    year: 2014,
-}
 
-let mockRows: dashboardData[] = [row1, row2, row3, row1, row2];
+let mockRows: dashboardData[] = [row1];
 
 
 function DashboardTable(): JSX.Element{
@@ -37,47 +26,64 @@ function DashboardTable(): JSX.Element{
     let darkRow: Boolean = false;
     let darkRowClass: string = "bg-purple-100";
 
+    const [windowTrigger, setWindowTrigger] = useState<Boolean>(false);
+
+    function handleDetailsClick(id: number){
+        setWindowTrigger(true);
+    }
+
+    function handleFinishClick(){
+        //opens pop up to ask if sure if finished
+    }
+
+    
+
     return(
-        <table className="table-style">
-                    <thead>
-                        <tr>
-                            <th className="table-head-cell">Brand</th>
-                            <th className="table-head-cell">Model</th>
-                            <th className="table-head-cell">Due Date</th>
-                            <th className="table-head-cell">Year</th>
-                            <th className="table-head-cell">Details</th>
-                            <th className="table-head-cell">Finish</th>
-                        </tr>
-                    </thead>
+        <div>
+            {
+                windowTrigger ? <BlackBackground windowTrigger={windowTrigger} setWindowTrigger={setWindowTrigger}/> : null
+            }
+            <table className="table-style">
+                 <thead>
+                    <tr>
+                        <th className="table-head-cell">Brand</th>
+                        <th className="table-head-cell">Model</th>
+                        <th className="table-head-cell">Due Date</th>
+                        <th className="table-head-cell">Year</th>
+                        <th className="table-head-cell">Details</th>
+                        <th className="table-head-cell">Finish</th>
+                    </tr>
+                </thead>
 
-                    <tbody>
-                    {
-                    mockRows.map((row) => {
-
-                        if(darkRow === false){
-                            darkRow = true;
-                            darkRowClass = "bg-purple-100";
-                        } else if(darkRow === true){
-                            darkRow = false;
-                            darkRowClass = "";
-                        }
-
-                        return(
-                            <tr>
-                                <td className={"table-cell " + darkRowClass}>{row.brand}</td>
-                                <td className={"table-cell " + darkRowClass}>{row.model}</td>
-                                <td className={"table-cell " + darkRowClass}>{row.dueDate}</td>
-                                <td className={"table-cell " + darkRowClass}>{row.year}</td>
-                                <td className={"table-cell " + darkRowClass}>OPEN</td>
-                                <td className={"table-cell " + darkRowClass}>FINISH</td>
-                            </tr>
-                        )
-                    })
+                <tbody>
+                {
+                mockRows.map((row) => {
+                    if(darkRow === false){
+                        darkRow = true;
+                        darkRowClass = "bg-purple-100";
+                    } else if(darkRow === true){
+                        darkRow = false;
+                        darkRowClass = "";
                     }
-                    </tbody>
-                    
-
-                </table>
+                    return(
+                        <tr key={row.id}>
+                            <td className={"table-cell " + darkRowClass}>{row.brand}</td>
+                            <td className={"table-cell " + darkRowClass}>{row.model}</td>
+                            <td className={"table-cell " + darkRowClass}>{row.dueDate}</td>
+                            <td className={"table-cell " + darkRowClass}>{row.year}</td>
+                            <td className={"table-cell " + darkRowClass}>
+                                <h1 className="table-button" onClick={() => handleDetailsClick(row.id)}>Details</h1>
+                            </td>
+                            <td className={"table-cell " + darkRowClass}>
+                                <h1 className="table-button">Finish</h1>
+                            </td>
+                        </tr>
+                    )
+                })
+                }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
