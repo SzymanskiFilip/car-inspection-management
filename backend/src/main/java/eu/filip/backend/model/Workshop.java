21ebbee9c0bc308bde239babbf58f92cc68f3f42
Filip <szymanski.fp@gmail.com>
2022-04-22 22:28:02 +0200

    added Workshop pojo

--- backend/src/main/java/eu/filip/backend/model/Workshop.java
+++ backend/src/main/java/eu/filip/backend/model/Workshop.java
@@ -1,4 +1,22 @@
 package eu.filip.backend.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Table(name = "workshops")
+@Data
 public class Workshop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long workshop_id;
+
+    @Column(name = "fk_user_id")
+    private Long fk_user_id;
+
+    @Column(name = "fk_username")
+    private Long fk_username;
+
+    @Column(name = "workshop_name")
+    private Long workshop_name;
 }
