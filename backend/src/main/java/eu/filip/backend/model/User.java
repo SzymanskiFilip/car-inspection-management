c63a09ed3450a535cf040db48fdbe5d5bc2fa198
Filip <szymanski.fp@gmail.com>
2022-04-22 22:22:08 +0200

    modelled User pojo

--- backend/src/main/java/eu/filip/backend/model/User.java
+++ backend/src/main/java/eu/filip/backend/model/User.java
@@ -1,4 +1,35 @@
 package eu.filip.backend.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(name = "username")
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @Column(name = "password")
+    @NotNull
+    @NotBlank
+    private String password;
+
+    @Column(name = "email")
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @Column(name = "role")
+    @NotNull
+    @NotBlank
+    private String role;
 }
