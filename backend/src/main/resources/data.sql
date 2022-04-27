7f9b6a13a9ba71959926a1b82ca3c216495ff9db
Filip <szymanski.fp@gmail.com>
2022-04-27 21:56:22 +0200

    rework

--- backend/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into users (username, password, role) values ('workshop133', '1234', 'workshop');
-insert into workshops(user_id, name) values (1, 'Werkstatt 21');
-
-insert into users (username, password, role) values ('client133', '123', 'client');
-insert into clients (first_name, last_name, user_id, workshop_id) values ('Filip', 'Szymanski', 2, 1);
-
-insert into inspections (client_id, workshop_id) values (1, 1);
-insert into todo_points (inspection_id, body, status) values (1, 'Check Lights', false);
